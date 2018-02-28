71f13fcb40b08d36a505a90548187abfa62bc4fc
Saul Moreno Abril <sam6.13.93@hotmail.com>
2018-02-28 19:18:05 +0100

    WIP: Tutorial 1 Completed

---
+++ Intermediate/Build/Mac/UE4Editor/Inc/FPSProject/FPSProjectGameModeBase.gen.cpp
@@ -0,0 +1,69 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "FPSProjectGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeFPSProjectGameModeBase() {}
+// Cross Module References
+	FPSPROJECT_API UClass* Z_Construct_UClass_AFPSProjectGameModeBase_NoRegister();
+	FPSPROJECT_API UClass* Z_Construct_UClass_AFPSProjectGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_FPSProject();
+// End Cross Module References
+	void AFPSProjectGameModeBase::StaticRegisterNativesAFPSProjectGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_AFPSProjectGameModeBase_NoRegister()
+	{
+		return AFPSProjectGameModeBase::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AFPSProjectGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+				(UObject* (*)())Z_Construct_UPackage__Script_FPSProject,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+				{ "IncludePath", "FPSProjectGameModeBase.h" },
+				{ "ModuleRelativePath", "FPSProjectGameModeBase.h" },
+				{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<AFPSProjectGameModeBase>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&AFPSProjectGameModeBase::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00900288u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AFPSProjectGameModeBase, 126949343);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AFPSProjectGameModeBase(Z_Construct_UClass_AFPSProjectGameModeBase, &AFPSProjectGameModeBase::StaticClass, TEXT("/Script/FPSProject"), TEXT("AFPSProjectGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AFPSProjectGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
